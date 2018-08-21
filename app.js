385a563707e2f40820af75759e76499fb3337ba1
Minsuk Chang <mchang@adobe.com>
2018-08-20 18:47:50 -0700

    converting words into numbers

--- app.js
+++ app.js
@@ -77,7 +77,11 @@ recognition.onresult = function(event) {
   if (transcript.indexOf("skip") != -1 && transcript.indexOf("seconds") != -1 ) {
     //console.log("skipping ahead..")
     var seconds = command.split(" ")[1];
+    if (Number.isInteger(seconds) == false ){
+      seconds=text2num(seconds);
+    }   
     document.getElementById('skip-btn').value = seconds;
+    
 
     //console.log(document.querySelector('skip').value);
     document.getElementById('skip-btn').click();
@@ -87,7 +91,11 @@ recognition.onresult = function(event) {
   if (transcript.indexOf("skip") != -1 && transcript.indexOf("minutes") != -1 ) {
     //console.log("Volume down..")
     var minutes = command.split(" ")[1];
+    if (Number.isInteger(minutes) == false){
+      minutes = text2num(minutes);
+    }
     document.getElementById('skip-btn').value = minutes * 60;
+
 
     //console.log(document.querySelector('skip').value);
     document.getElementById('skip-btn').click();
@@ -172,3 +180,84 @@ $('#pause-btn').on('click', function(e) {
   recognition.abort();
   //console.log('Voice recognition paused.');
 });
+
+
+//chars to numbers
+var Small = {
+  'zero': 0,
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6,
+  'seven': 7,
+  'eight': 8,
+  'nine': 9,
+  'ten': 10,
+  'eleven': 11,
+  'twelve': 12,
+  'thirteen': 13,
+  'fourteen': 14,
+  'fifteen': 15,
+  'sixteen': 16,
+  'seventeen': 17,
+  'eighteen': 18,
+  'nineteen': 19,
+  'twenty': 20,
+  'thirty': 30,
+  'forty': 40,
+  'fifty': 50,
+  'sixty': 60,
+  'seventy': 70,
+  'eighty': 80,
+  'ninety': 90
+};
+
+var Magnitude = {
+  'thousand':     1000,
+  'million':      1000000,
+  'billion':      1000000000,
+  'trillion':     1000000000000,
+  'quadrillion':  1000000000000000,
+  'quintillion':  1000000000000000000,
+  'sextillion':   1000000000000000000000,
+  'septillion':   1000000000000000000000000,
+  'octillion':    1000000000000000000000000000,
+  'nonillion':    1000000000000000000000000000000,
+  'decillion':    1000000000000000000000000000000000,
+};
+
+var a, n, g;
+
+function text2num(s) {
+  a = s.toString().split(/[\s-]+/);
+  n = 0;
+  g = 0;
+  a.forEach(feach);
+  return n + g;
+}
+
+function feach(w) {
+  var x = Small[w];
+  if (x != null) {
+      g = g + x;
+  }
+  else if (w == "hundred") {
+      g = g * 100;
+  }
+  else {
+      x = Magnitude[w];
+      if (x != null) {
+          n = n + g * x
+          g = 0;
+      }
+      else { 
+          alert("Unknown number: "+w); 
+      }
+  }
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
